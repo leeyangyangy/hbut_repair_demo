3b85192737d79d0572f591ecb81f3867e420e554
LEEYANGYANG <liyangyang0713@foxmail.com>
2022-11-03 01:20:11 +0800

    jwt和放行页面

---
+++ src/main/java/com/lyy/manager/framework/constants/AuthHeadConstant.java
@@ -0,0 +1,15 @@
+package com.lyy.manager.framework.constants;
+
+/**
+ * @Package: com.lyy.security.framwork.constants
+ * @Author: LEEYANGYANG
+ * @Create: 2022/11/2 20:00
+ * @Description:
+ */
+public interface  AuthHeadConstant {
+
+    /**
+     * 表头
+     */
+    String Authorization = "Authorization";
+}
---
+++ src/main/java/com/lyy/manager/framework/constants/SecurityConstant.java
@@ -0,0 +1,15 @@
+package com.lyy.manager.framework.constants;
+
+/**
+ * @Package: com.lyy.security.framwork.constants
+ * @Author: LEEYANGYANG
+ * @Create: 2022/11/2 19:54
+ * @Description:
+ */
+public interface SecurityConstant {
+
+    String[] SECURITY_URL = {
+
+            "/login/**"
+    };
+}
