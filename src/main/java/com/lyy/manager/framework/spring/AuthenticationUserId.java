fd60d89bbfdb73e4734fe8a1426460dccc9b1b89
leeyangyang <liyangyang0713@foxmail.com>
2022-11-19 15:33:05 +0800

    响应非法请求

--- src/main/java/com/lyy/manager/framework/spring/AuthenticationUserId.java
+++ src/main/java/com/lyy/manager/framework/spring/AuthenticationUserId.java
@@ -3,23 +3,21 @@ package com.lyy.manager.framework.spring;
 
 import cn.hutool.core.util.ObjectUtil;
 import com.lyy.manager.framework.constants.AuthHeadConstant;
+import com.lyy.manager.framework.exception.ServiceException;
+import com.lyy.manager.framework.exception.enums.AuthExceptionEnum;
 import com.lyy.manager.framework.jwt.JwtUtils;
-import com.lyy.manager.framework.respone.ResponseData;
-import org.springframework.stereotype.Component;
+import com.lyy.manager.framework.respone.ResponseUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@Component
 public class AuthenticationUserId implements HandlerInterceptor {
     //        添加处理
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("预拦截");
-        ResponseData responseData = new ResponseData();
-        responseData.setCode(403);
 //        获取用户信息
 //        获取当前认证token信息
         String token = request.getHeader(AuthHeadConstant.Authorization);
@@ -39,7 +37,11 @@ public class AuthenticationUserId implements HandlerInterceptor {
 //            System.out.println("非空  放行，true，继续后面操作");
             return true;
         } else {
+//            响应拦截错误信息
+            ResponseUtils.responseExceptionError(response, 403, "非法操作",
+                    new ServiceException(AuthExceptionEnum.REQUEST_TOKEN_ERROR).getStackTrace()[0].toString());
 //            System.out.println("非法操作，false，拦截");
+//            return false不允许进行下一步操作
             return false;
         }
 
