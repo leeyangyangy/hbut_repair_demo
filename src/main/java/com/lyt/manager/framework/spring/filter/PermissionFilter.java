84277b3c3fae14048c35acbd9de1c6f2205f06f9
leeyangy <liyangyang0713@foxmail.com>
2022-11-29 00:03:29 +0800

    配置过滤器
    Signed-off-by: leeyangy <liyangyang0713@foxmail.com>

--- src/main/java/com/lyt/manager/framework/spring/filter/PermissionFilter.java
+++ src/main/java/com/lyt/manager/framework/spring/filter/PermissionFilter.java
@@ -1,12 +1,20 @@
-package com.lyt.manager.framework.spring;
+package com.lyt.manager.framework.spring.filter;
 
+import com.lyt.manager.modular.system.pojo.Permission;
+import com.lyt.manager.modular.system.service.PermissionService;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.Resource;
 import javax.servlet.*;
 import java.io.IOException;
+import java.util.List;
 
 @Configuration("permissionFilter")
 public class PermissionFilter implements Filter {
+
+    @Resource
+    private PermissionService permissionService;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         System.out.println("---- permissionFilter init ----");
@@ -16,7 +24,12 @@ public class PermissionFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("---- permissionFilter doFilter ----");
-
+        List<Permission> permissionList = permissionService.findAll();
+        servletRequest.setAttribute("permissionList", permissionList);
+        for (Permission permission:permissionList)
+            System.out.println(permission);
+//        请求向下传递
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
