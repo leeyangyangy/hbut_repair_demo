f7ef3c85070d5bbe5f162c99cd941f6dabbf74af
LEEYANGYANG <1787294587@qq.com>
2022-12-04 00:09:41 +0800

    分类crud

--- src/main/java/com/lyt/manager/modular/system/mapper/TypeMapper.java
+++ src/main/java/com/lyt/manager/modular/system/mapper/TypeMapper.java
@@ -3,9 +3,11 @@ package com.lyt.manager.modular.system.mapper;
 import com.lyt.manager.modular.system.pojo.Type;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.List;
+
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author lyy
@@ -13,4 +15,21 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface TypeMapper extends BaseMapper<Type> {
 
+    //    查询全部种类（小类别）
+    List<Type> findAllType();
+
+    //    添加类别（小类别）
+    int addType(Type type);
+
+    //    删除类别（小类别）
+    int delTypeById(Integer id);
+
+    //    按名查找
+    Type findTypeByName(String typeName);
+
+    //    按名修改
+    int updateTypeByName(Type type);
+
+//    按id查找
+    Type findTypeById(Integer id);
 }
