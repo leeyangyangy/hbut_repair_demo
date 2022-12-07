e58676f51de97a2d99d9d80acfe43379205ae9ac
LEEYANGYANG <1787294587@qq.com>
2022-12-08 03:36:41 +0800

    添加模糊分组查询接口

--- src/main/java/com/lyt/manager/modular/system/mapper/DeliveryMapper.java
+++ src/main/java/com/lyt/manager/modular/system/mapper/DeliveryMapper.java
@@ -2,6 +2,7 @@ package com.lyt.manager.modular.system.mapper;
 
 import com.lyt.manager.modular.system.pojo.Delivery;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lyt.manager.modular.system.pojo.User;
 
 import java.util.List;
 
@@ -19,4 +20,7 @@ public interface DeliveryMapper extends BaseMapper<Delivery> {
 
     //    插入记录
     int addDelivery(Delivery delivery);
+
+//    按姓名模糊查询
+    List<Delivery> findDeliveryByLikeUserName(String name);
 }
